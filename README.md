2ceb54dfbfbdaeaa66c634d7a142244cd3451b8b
nicko858 <ikar2021@hotmail.com>
2018-07-23 14:42:16 +0300

    Script  github_trending.py & readme.md

--- README.md
+++ README.md
@@ -1,6 +1,80 @@
 # Github Trends
 
-[TODO. There will be project description]
+The script provides data of the most github's trending repositories for the last week.
+
+# Quickstart
+
+The program is represented by the module github_trending.py. Module github_trending.py contains the following functions:
+
+- ```get_repo_name()```
+- ```get_repo_open_issues_count()```
+- ```get_repo_owner()```
+- ```get_repo_url()```
+- ```get_stars_count()```
+- ```get_trending_repositories()```
+- ```make_data_to_print()```
+- ```print_data()```
+
+The program uses these libs from Python standart and third-party libraries:
+
+```json```
+```datetime```
+```request```
+
+**How in works:**
+
+- The program connects to the https://github.com/ 
+- Using ```search/repositories```- method, gets top-20 -data trending repositories of the last week (The trend repository has a lot of stars)
+- Prints result in human-readble format  
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+
+# How to run
+- Activate virtualenv
+``` bash
+source <path_to_virtualenv>/bin/activate
+```
+- Run script with virtualenv-interpreter
+```bash
+<path_to_virtualenv>/bin/python3.5 github_trending.py
+```
+If everything is fine, you'll see such output:
+```text
+The most trending github repositories for the period from 2018-07-16 to 2018-07-23:
+
+******************************************************************************************
+repo_name: ru-web-developer-security-checklist
+repo_url: minotaura
+repo_owner: https://github.com/minotaura/ru-web-developer-security-checklist
+stars: 84
+opened_issues_count: 0
+******************************************************************************************
+repo_name: WanAndroid
+repo_url: jenly1314
+repo_owner: https://github.com/jenly1314/WanAndroid
+stars: 98
+opened_issues_count: 0
+******************************************************************************************
+```
+
+In case of github unavaible, you'll see this message:
+```text
+The service https://api.github.com/ is unavailable!
+```
+
+In case of wrong data format, you'll see this message:
+```text
+Invalid data returned! Check input parameters!
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,105 @@
+import requests
+from requests import Timeout
+from requests import ConnectionError
+from datetime import date
+from datetime import timedelta
+import json
+from json import JSONDecodeError
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(api_url, top_size, interval_a, interval_b):
+    method = "search/repositories"
+    query = "created:{}..{}".format(interval_b, interval_a)
+    target_url = ("{}{}?q={}&sort=stars&per_page={}".format(
+        api_url,
+        method,
+        query,
+        top_size
+    ))
+    try:
+        response = requests.get(target_url)
+        return response.text
+    except (Timeout, ConnectionError):
+        return None
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_stars_count(repo):
+    return repo["stargazers_count"]
 
-if __name__ == '__main__':
-    pass
+
+def get_repo_name(repo):
+    return repo["name"]
+
+
+def get_repo_owner(repo):
+    return repo["owner"]["login"]
+
+
+def get_repo_url(repo):
+    return repo["html_url"]
+
+
+def get_repo_open_issues_count(repo):
+    return repo["open_issues"]
+
+
+def make_data_to_print(repos):
+    try:
+        parsed_data = json.loads(repos)
+        data_to_print = {}
+        repos = parsed_data["items"]
+        for repo in repos:
+            repo_name = get_repo_name(repo)
+            repo_url = get_repo_url(repo)
+            repo_owner = get_repo_owner(repo)
+            stars = get_stars_count(repo)
+            open_issues_count = get_repo_open_issues_count(repo)
+            data_to_print[repo_name] = [
+                repo_owner,
+                repo_url,
+                stars,
+                open_issues_count
+            ]
+        return data_to_print
+    except JSONDecodeError:
+        return None
+
+
+def print_data(data_to_print, interval_a, interval_b):
+    delimiter = "*" * 90
+    print("\nThe most trending github repositories"
+          " for the period from {} to {}:\n".format(
+           interval_b,
+           interval_a
+          ))
+    for repo_name, repo_info in data_to_print.items():
+        print(delimiter)
+        repo_url, repo_owner, stars, opened_issues_count = repo_info
+        print("repo_name: {}\n"
+              "repo_url: {}\nrepo_owner: {}\nstars: {}\n"
+              "opened_issues_count: {}".format(
+                  repo_name,
+                  repo_url,
+                  repo_owner,
+                  stars,
+                  opened_issues_count
+              ))
+
+
+if __name__ == "__main__":
+    interval_a = date.today()
+    interval_b = interval_a - timedelta(days=7)
+    api_url = "https://api.github.com/"
+    top_size = 20
+    trending_repos = get_trending_repositories(
+        api_url,
+        top_size,
+        interval_a,
+        interval_b
+    )
+    if not trending_repos:
+        exit("The service '{}' is unavailable!".format(api_url))
+    data_to_print = make_data_to_print(trending_repos)
+    if not data_to_print:
+        exit("Invalid data returned! Check input parameters!")
+    print_data(data_to_print, interval_a, interval_b)
